73b85de4b58d904fe6d6408a58a3153095b21dd2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-21 12:31:05 +0300

    Added a keyframes section from the open-menu animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       screens: {
         'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
         'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)' },
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        }
       }
     },
   },
